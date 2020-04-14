2224c277816e51f14dc7e8d33086da08977c4472
Bobby Durrett <bobby@bobbydurrettdba.com>
2020-04-14 16:44:29 -0700

    Consolidated sqlstat scripts

--- awr/sqlstat.sql
+++ awr/sqlstat.sql
@@ -20,26 +20,49 @@ set trimspool on
 
 spool &ns.sqlstat.log
 
+-- Replaces all the earlier versions of sqlstat.sql
+-- including sqlstat2.sql and sqlstat3.sql
+
 column END_INTERVAL_TIME format a25
 
-select ss.sql_id,
+-- g0wqzdhxy62mc
+
+select 
+sql_id,
+plan_hash_value,
+END_INTERVAL_TIME,
+executions_delta,
+ELAPSED_TIME_DELTA/(nonzeroexecutions*1000) "Elapsed Average ms",
+CPU_TIME_DELTA/(nonzeroexecutions*1000) "CPU Average ms",
+IOWAIT_DELTA/(nonzeroexecutions*1000) "IO Average ms",
+CLWAIT_DELTA/(nonzeroexecutions*1000) "Cluster Average ms",
+APWAIT_DELTA/(nonzeroexecutions*1000) "Application Average ms",
+CCWAIT_DELTA/(nonzeroexecutions*1000) "Concurrency Average ms",
+BUFFER_GETS_DELTA/nonzeroexecutions "Average buffer gets",
+DISK_READS_DELTA/nonzeroexecutions "Average disk reads",
+ROWS_PROCESSED_DELTA/nonzeroexecutions "Average rows processed"
+from
+(select 
+ss.snap_id,
+ss.sql_id,
 ss.plan_hash_value,
 sn.END_INTERVAL_TIME,
 ss.executions_delta,
-ELAPSED_TIME_DELTA/(executions_delta*1000) "Elapsed Average ms",
-CPU_TIME_DELTA/(executions_delta*1000) "CPU Average ms",
-IOWAIT_DELTA/(executions_delta*1000) "IO Average ms",
-CLWAIT_DELTA/(executions_delta*1000) "Cluster Average ms",
-APWAIT_DELTA/(executions_delta*1000) "Application Average ms",
-CCWAIT_DELTA/(executions_delta*1000) "Concurrency Average ms",
-BUFFER_GETS_DELTA/executions_delta "Average buffer gets",
-DISK_READS_DELTA/executions_delta "Average disk reads",
-ROWS_PROCESSED_DELTA/executions_delta "Average rows processed"
+case ss.executions_delta when 0 then 1 else ss.executions_delta end nonzeroexecutions,
+ELAPSED_TIME_DELTA,
+CPU_TIME_DELTA,
+IOWAIT_DELTA,
+CLWAIT_DELTA,
+APWAIT_DELTA,
+CCWAIT_DELTA,
+BUFFER_GETS_DELTA,
+DISK_READS_DELTA,
+ROWS_PROCESSED_DELTA
 from DBA_HIST_SQLSTAT ss,DBA_HIST_SNAPSHOT sn
-where ss.sql_id = 'gafr2s72v664b'
+where ss.sql_id = 'g0wqzdhxy62mc'
 and ss.snap_id=sn.snap_id
-and executions_delta > 0
-and ss.INSTANCE_NUMBER=sn.INSTANCE_NUMBER
-order by ss.snap_id,ss.sql_id;
+and ss.INSTANCE_NUMBER=sn.INSTANCE_NUMBER)
+where ELAPSED_TIME_DELTA > 0
+order by snap_id,sql_id;
 
 spool off
